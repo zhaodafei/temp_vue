93ebc3d7b9ee8af75342065f1faca388eda2d3b2
zhaodafei111 <1097625354@qq.com>
2022-08-19 22:23:53 +0800

    no message

--- vue.config.js
+++ vue.config.js
@@ -7,6 +7,7 @@ module.exports = {
      * 例如 https://www.fei.com/。如果应用被部署在一个子路径上，你就需要用这个选项指定这个子路径。
      * 例如，如果你的应用被部署在 https://www.fei.com/admin/，则设置 baseUrl 为 /admin/。
      * */
+    // publicPath: process.env.NODE_ENV === 'production' ? './' : '/',
     // publicPath: './',
     publicPath: '/', // 历史模式中不能加点
     // 打包输出文件目录
